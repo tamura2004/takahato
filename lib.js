e653c6a9926ed80464f3befa9bb6476714981dc7
tamura <tamura2004@gmail.com>
2019-12-09 21:53:08 +0900

    Add takahato

--- lib.js
+++ lib.js
@@ -1,95 +1,100 @@
-function easing(x) {
-  return x;
-  // return (x * 2 -1) ** 0.333;
+class Creature {
+  constructor(type) {
+    this.p = createVector(random(windowWidth), random(windowHeight));
+    this.v = p5.Vector.random2D().mult(2);
+    this.type = type;
+    this.color = type === "HATO" ? "#0074bf" : "#c93a40";
+    this.size = R;
+    this.id = random(100000000);
+  }
+  move() {
+    // １％の確率で右に曲がる
+    if (random() < 0.01) this.v.rotate(HALF_PI);
+
+    // HATOならランダムに大きくなる
+    if (random() < 0.01 && this.type === "HATO") this.size *= 1.05;
+
+    // 一つ進む
+    this.p.add(this.v);
+
+    // 壁に当たったら跳ね返る
+    if (this.p.x < R && this.v.x < 0) this.v.x *= -1;
+    if (this.p.x > windowWidth - R * 3 && this.v.x > 0) this.v.x *= -1;
+    if (this.p.y < R && this.v.y < 0) this.v.y *= -1;
+    if (this.p.y > windowHeight - R * 3 && this.v.y > 0) this.v.y *= -1;
+  }
+  collid(other) {
+    // 衝突したら向きをランダムに変える
+    this.v.rotate(random(PI));
+    other.v.rotate(random(PI));
+
+    if (this.type === "TAKA") {
+      if (other.type === "TAKA") {
+        this.size *= 0.95;
+        other.size *= 0.95;
+      } else {
+        if (this.size < R * 3) this.size *= 1.05;
+        other.size *= 0.95;
+      }
+    } else {
+      if (other.type === "HATO") {
+        if (this.size < R * 2) this.size *= 1.05;
+        if (other.size < R * 2) other.size *= 1.05;
+      } else {
+        if (other.size < R * 3) other.size *= 1.05;
+        this.size *= 0.95;
+      }
+    }
+  }
+  draw() {
+    fill(this.color);
+    circle(this.p.x, this.p.y, this.size);
+  }
 }
 
 class Creatures extends Map {
   add(creature) {
     this.set(creature.id, creature);
   }
+  create() {
+    const type = random() < 0.3 ? "TAKA" : "HATO";
+    const creature = new Creature(type);
+    this.add(creature);
+  }
+  clone(parent) {
+    if (this.values().length > 100) return;
+    parent.size *= 0.8;
+    const child = new Creature(parent.type);
+    child.p = p5.Vector.random2D().mult(R*2).add(parent.p);
+    this.add(child);
+  }
   remove(creature) {
     this.delete(creature.id);
   }
   move() {
     for (const creature of this.values()) {
+      if (creature.size < 16) this.remove(creature);
+      if (creature.size > R * 2.5 && random() < 0.01) this.clone(creature);
       creature.move();
     }
     this.collid();
   }
   collid() {
-    for (const c1 of this.values()) {
-      for (const c2 of this.values()) {
-        if (c1.id !== c2.id && c1.p.dist(c2.p) <= R) {
-          if (c1.type === "TAKA" && c2.type === "TAKA") {
-            this.remove(c2);
-          // } else if (c1.type == "HATO" && c2.type == "HATO") {
-          //   const child = new Creature("HATO");
-          //   child.p.x = c1.p.x;
-          //   child.p.y = c1.p.y;
-          //   this.add(child);
-          // } else if (c1.type === "TAKA" && c2.type === "HATO") {
-          //   c2.path = [];
-          //   const child = new Creature("HATO");
-          //   child.p.x = c1.p.x;
-          //   child.p.y = c1.p.y;
-          //   this.add(child);
-          // } else {
-          //   c1.path = [];
-          //   const child = new Creature("HATO");
-          //   child.p.x = c1.p.x;
-          //   child.p.y = c1.p.y;
-          //   this.add(child);
-          }
-        }
+    for (let c1 of this.values()) {
+      for (let c2 of this.values()) {
+        if (c1.id === c2.id) continue;
+        if (c1.p.dist(c2.p) < R) c1.collid(c2);
       }
     }
   }
   draw() {
-    for (const creature of this.values()) {
-      creature.draw();
-    }
+    for (const creature of this.values()) creature.draw();
+  }
+  taka() {
+    return this.values.filter(c => c.type === "TAKA");
+  }
+  hato() {
+    return this.values.filter(c => c.type === "HATO");
   }
 }
 
-class Creature {
-  constructor(type) {
-    this.p = createVector(random(windowWidth), random(windowHeight));
-    this.path = [];
-    this.wait = random(100);
-    this.id = random(100000000);
-    this.type = type;
-  }
-  goto(q) {
-    for (let i = 1; i <= STEP; i++) {
-      const x = i / STEP;
-      this.path.push(p5.Vector.lerp(this.p, q, easing(x)));
-    }
-  }
-  move() {
-    if (this.path.length > 0) {
-      // １フレーム移動
-      this.p = this.path.shift();
-    } else if (this.wait > 0) {
-      // その場で待つ
-      this.wait--;
-    } else {
-      // STEPフレームの移動を決める
-      const x = random(R, windowWidth - R);
-      const y = random(R, windowHeight - R);
-      const q = createVector(x, y);
-      this.goto(q);
-
-      // 待ち時間を決める
-      this.wait = random(MAX_WAIT);
-    }
-  }
-  draw() {
-    // line(0, 0, this.p.x, this.p.y);
-    if (this.type == "HATO") {
-      fill('#0074bf');
-    } else {
-      fill('#c93a40');
-    }
-    circle(this.p.x, this.p.y, R);
-  }
-}
--- sketch.js
+++ sketch.js
@@ -2,26 +2,17 @@
 const STEP = 300;
 const R = 32;
 const MAX_WAIT = 30;
-const NUM = 10;
+const NUM = 40;
 
 let creatures = new Creatures();
-let creature;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for (let i = 0; i < NUM; i++) {
-    creature = new Creature();
-    creature.type = "HATO"
-    creatures.add(creature);
-  }
-  for (let i = 0; i < NUM; i++) {
-    creature = new Creature();
-    creature.type = "TAKA"
-    creatures.add(creature);
-  }
+  for (let i = 0; i < NUM; i++) creatures.create();
 }
 
 function draw() {
+  if (random() < 0.005) creatures.create();
   background(255);
   creatures.move();
   creatures.draw();
